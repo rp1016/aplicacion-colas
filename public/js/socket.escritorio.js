28428ef228fd9a86c75f4064e2c5c5fcded4951c
PARRR028 <rafael.x.parra.-nd@disney.com>
2019-04-24 20:30:23 -0500

    Primer commit

---
+++ public/js/socket.escritorio.js
@@ -0,0 +1,34 @@
+// comunicacion activa con el servidor. 
+var socket = io();
+
+
+//search params
+var searchParams = new URLSearchParams(window.location.search);
+if (!searchParams.has('escritorio')) {
+
+    window.location = 'index.html';
+    throw new Error('El escritorio es necesario');
+}
+
+var escritorio = searchParams.get('escritorio');
+var label = $('small');
+
+$('h1').text('Escritorio: ' + escritorio);
+
+//listener del boton
+$('button').on('click', function() {
+
+
+    socket.emit('atenderTicket', { escritorio: escritorio }, function(resp) {
+
+        if (resp === 'No hay tickets') {
+
+            label.text(resp);
+            alert(resp);
+            return;
+        }
+
+        label.text('Ticket' + resp.numero);
+    });
+
+});
